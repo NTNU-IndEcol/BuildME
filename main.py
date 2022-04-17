759d883098c1e8c4badce118311764addb4d9f40
Niko Heeren <niko.heeren@gmail.com>
2022-04-17 19:34:49 +0200

    Love for main.py - Now has two functions.   - One to run from scratch and   - one to continue a previous run (that crashed). - Some text :)

--- main.py
+++ main.py
@@ -1,33 +1,64 @@
-
-import pandas as pd
-import numpy as np
-from tqdm import tqdm
-import encodings
-
 from BuildME import settings, pre, idf, material, energy, simulate, __version__
 
-# Some pre-processing
-pre.validate_ep_version()
-pre.create_mmv_variants(comb=settings.debug_combinations)
 
-# Creating the corresponding .idf file given an energy standard and a resource efficiency strategy
-#  and copy file into folder
-simulation_files, run = simulate.create_combinations(settings.debug_combinations)
+def run_new():
+    """Run new simulation from scratch"""
 
-#deletes exisitng folders that contains simulation results
-# simulate.nuke_folders(simulation_files)  # deletes only the folder with the case you try to simulate
+    print("Running new simulation...")
+    # Some pre-processing
+    pre.validate_ep_version()
+    pre.create_mmv_variants(comb=settings.debug_combinations)
 
-# Copy scenarios .idf to the correct folder
-simulate.copy_scenario_files(simulation_files, run)
-# Create run file
-# simulate.create_sq_job(simulation_files)
+    # Creating the scenario combinations based on energy standard and a resource efficiency strategy
+    #   as specified in settings.combinations
+    fnames, run = simulate.create_combinations(settings.debug_combinations)
 
-simulate.calculate_energy()
-simulate.calculate_materials(run, simulation_files)
+    # Deletes exisitng folders that contains simulation results
+    # simulate.nuke_folders(fnames)  # deletes only the folder with the case you try to simulate
 
-res_energy = simulate.collect_energy(simulation_files)
-res_mat = simulate.load_material(simulation_files)
+    # Copy scenarios .idf to ./tmp
+    simulate.copy_scenario_files(fnames, run)
+
+    # Create run file for HPC
+    # simulate.create_sq_job(fnames)
+
+    simulate.calculate_energy()
+    simulate.calculate_materials(run, fnames)
+
+    res_energy = simulate.collect_energy(fnames)
+    res_mat = simulate.load_material(fnames)
+
+    simulate.save_ei_result(run, res_energy, res_mat)
+    simulate.save_mi_result(run, res_mat)
 
 
-simulate.save_ei_result(run, res_energy, res_mat)
-simulate.save_mi_result(run, res_mat)
+def continue_previous(run_eplus=False):
+    """
+    Continue a previous simulation. Assumes that scenarios were created and ./tmp folder was created. Will load latest
+    subfolder (using the name) in the .tmp/ folder. This is especially useful when the energy simulation has already
+    been performed successfully, because this is the most time-consuming step.
+    :param run_eplus: If True, energy simulation will be performed
+    :return: None
+    """
+
+    # Find and load last simulation
+    print("Continuing previous simulation...")
+    fnames, run = simulate.find_last_run()
+    fnames = simulate.load_run_data_file(fnames)
+    if run_eplus:
+        # if energy simulation was not successful yet
+        simulate.calculate_energy()
+    simulate.calculate_materials(run, fnames)
+
+    res_energy = simulate.collect_energy(fnames)
+    res_mat = simulate.load_material(fnames)
+
+    simulate.save_ei_result(run, res_energy, res_mat)
+    simulate.save_mi_result(run, res_mat)
+
+
+if __name__ == "__main__":
+    # Only run either of the following functions
+    # run_new()
+    continue_previous()
+    print("Done.")
